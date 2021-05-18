f532fa9c2380ab82ef73b5b9a43f72ac7d881a03
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-05-18 13:26:20 +0200

    Initial Commit

---
+++ MVPSimpleTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPSimpleTask
+//
+//  Created by Noor Walid on 18/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
